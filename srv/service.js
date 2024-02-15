381481f8a5d63fab0c0ab71ab814497993aef581
GANGA CHARITHA <ganga.charitha@peolsolutions.com>
2024-02-15 09:19:31 +0000

    first

--- srv/service.js
+++ srv/service.js
@@ -7,9 +7,9 @@ module.exports = cds.service.impl(async function () {
     // const c5re = await cds.connect.to('iflow1');
     this.on('READ', add_t.drafts, async (req,next) => {
         if(req.data.num1){
-        await SELECT.from(add_t.drafts);
+     var data3 =   await SELECT.from(add_t.drafts);
         console.log(req);
-        let n2 = req.data.num2
+        let n2 = data3[0].num2;
         let sum = req.data.num1 + n2
         await cds.update(add_t.drafts)
         .set({result:sum}) // Update with new data
